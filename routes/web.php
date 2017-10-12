7b63fb187e77500ad01e89a0bbf40a566541ef59
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-12 14:50:25 +0800

    另一個 Route 以群組包起來設定 prefix

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,11 @@
 //Route::get('/', function () {
     //return view('welcome');
 //});
-Route::get('dashboard',function(){
-    return 'dashboard';
+//Route::get('dashboard',function(){
+    //return 'dashboard';
+//});
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admindashboard';
+    });
 });
