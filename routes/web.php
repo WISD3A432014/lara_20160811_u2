8c067458282540af6b96a8609d24238009441574
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-12 14:28:08 +0800

    修改 Route 裡的路徑

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@
 //Route::get('hello/{name}',function($name){
     //return 'Hello,'.$name;
 //});
-Route::get('hello/{name?}',['as'=>'hello.index',function($name='Everybody'){
-    return'Hello,'.$name;
+Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
+    return view('welcome');
 });
 //Route::get('favorite/{singer}',function($singer){
     //return'favorite,'.$singer;
