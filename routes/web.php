21e1d8b12bd75461cb90a4d63de70fd070027587
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-12 14:23:26 +0800

    將 Route 取名為 hello.index

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@
 //Route::get('hello/{name}',function($name){
     //return 'Hello,'.$name;
 //});
-//Route::get('hello/{name?}',function($name='Everybody'){
-   // return'Hello,'.$name;
-//});
-Route::get('favorite/{singer}',function($singer){
-    return'favorite,'.$singer;
+Route::get('hello/{name?}',['as'=>'hello.index',function($name='Everybody'){
+    return'Hello,'.$name;
 });
+//Route::get('favorite/{singer}',function($singer){
+    //return'favorite,'.$singer;
+//});
