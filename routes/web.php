c7ccd862f747abd69da66ce453b2cf26b4a2a8d9
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-12 14:10:40 +0800

    修改參數成選擇性

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 //Route::get('/', function () {
    // return redirect('welcome');
 //});
-Route::get('hello/{name}',function($name){
-    return 'Hello,'.$name;
+//Route::get('hello/{name}',function($name){
+    //return 'Hello,'.$name;
+//});
+Route::get('hello/{name?}',function($name='Everybody'){
+    return'Hello,'.$name;
 });
